7d31aaf7792de4fce8ce8f317e77d5df27510bed
Rodrigo Jean (BJ) <rodrigo.j.fcamara@duratex.com.br>
2019-01-09 19:09:49 -0200

    Atualuzando comentarios e produtos em novo projeto. Ajuste tambem em Redis de Estrutura de codigo

--- produtos/produtos/src/Categoria.js
+++ produtos/produtos/src/Categoria.js
@@ -1,8 +1,46 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Categoria extends Component {
-  render() {
-    return <h1>Categoria Clicado</h1>    
+  constructor(props){
+    super(props)
+    this.loadData = this.loadData.bind(this)
+    this.state = { produtos: [], categoria:{} }
+  }
+  loadData(id){
+    axios
+    .get(`http://localhost:3001/produtos?categoria=${id}`)
+    .then(res => {
+      this.setState({ produtos: res.data })
+    })
+
+    axios
+    .get(`http://localhost:3001/categorias/${id}`)
+    .then(res => {
+      this.setState({ categoria: res.data })
+    })    
+  }
+  componentDidMount(){
+    const id = this.props.match.params.catId
+    this.loadData(id)
+  }
+
+  componentWillReceiveProps(newProps){
+    this.loadData(newProps.match.params.catId)
+  }
+  renderProduto(produto){
+    return (
+      <p className='card card-body bg-light' key={produto.id}>{produto.produto}</p>
+    )
+  }
+  render() {    
+    return(
+      <div>
+        <h1>{this.state.categoria.categoria} {/*this.props.match.params.catId*/}</h1>
+        <p></p>
+        {this.state.produtos.map(this.renderProduto)}
+      </div>
+    )
   }
 }
 
--- produtos/produtos/src/Produtos.js
+++ produtos/produtos/src/Produtos.js
@@ -2,17 +2,42 @@ import React, { Component } from 'react'
 import { Route, Link } from 'react-router-dom'
 import ProdutosHome from './ProdutosHome'
 import Categoria from './Categoria'
+import axios from 'axios'
 
 class Produtos extends Component {
+  constructor(props){
+    super(props)
+    this.state = { categorias: [] }
+  }
+  componentDidMount(){ //montado na tela
+    axios
+      .get('http://localhost:3001/categorias')
+      .then(res => {        
+        this.setState({ categorias: res.data })
+      })
+  }
+
+  renderCategoria(cat){
+    return (
+      <li key={cat.id}>
+        <Link to={`/produtos/categoria/${cat.id}`}>{cat.categoria}</Link>
+      </li>
+    )
+  }
+
   render(){
     
     const { match } = this.props
+    const { categorias } = this.state
 
     return(
       <div className='row'>
         <div>
           <h3>Categorias</h3>
-          <Link to='/produtos/categoria/1'> Categoria 1</Link>
+          <ul>
+          { categorias.map(cat => this.renderCategoria(cat) ) }
+          </ul>
+
         </div>
 
         <div className='col-md-10'>
