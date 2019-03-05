3f7fb2fc189554cf2dd22b4f0a03c5220812ba0d
RodrigoJeanSantos <rodrigojeansantos@hotmail.com>
2019-03-05 19:19:05 -0300

    Preparando estudos para Redux Saga

---
+++ redux/redux-saga-proj/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers/index'
+
+const store = createStore(reducers)
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App"></div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
