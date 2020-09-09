767b4210f32c6c1c829f279e84fa41409fa35a92
Rodrigo BJ <rodrigojeansantos@gmail.com>
2020-09-09 09:29:41 -0300

    Enviar atualizacao

--- Es678/map-intro.js
+++ Es678/map-intro.js
@@ -5,7 +5,19 @@ m.set('Rodrigo', 26)
 m.set('Jean', 28)
 m.set('Santos', 29)
 
-console.log(m, m.size, m.has('Rodrigo'), m.delete('Jean'))
+// console.log(m, m.size, m.has('Rodrigo'), m.delete('Jean'))
 const it = m.values()
-it.next()
-console.log(it)
+//console.log(m)
+//it.next()
+// console.log(it)
+
+// console.log(it)
+
+
+for (let a of m.keys()) {
+    // alert(vegetable); // cucumber, tomatoes, onion
+    console.log(it.next())
+  }
+
+
+
